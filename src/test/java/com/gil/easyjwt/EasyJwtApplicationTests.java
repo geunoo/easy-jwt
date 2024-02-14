4a8c3c28cc1e3c05966ca9dbaf059144358040bd
길근우 <kesksjxhx@gmail.com>
2024-02-14 15:51:29 +0900

    project init

---
+++ src/test/java/com/gil/easyjwt/EasyJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gil.easyjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
